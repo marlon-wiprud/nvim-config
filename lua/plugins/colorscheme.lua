b0bba1803648cf5cee26373ec384f027fb6e269c
Marlon Wiprud <marlonwiprud1@gmail.com>
2024-06-07 08:47:04 -0400

    setup colorscheme and comments

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   }
+-- }
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   }
+-- }
+return {
+  "folke/tokyonight.nvim",
+  lazy = true,
+  opts = { style = "moon", transparent = true },
+}
