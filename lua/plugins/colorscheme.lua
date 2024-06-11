9807190c6b5f0fd1fce074deebc82cfaf7a16005
Marlon Wiprud <marlon@marlonwiprud.com>
2024-06-11 06:27:50 -0400

    more language support

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "shaunsingh/nord.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
