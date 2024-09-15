cba8853444833d30a49e61b241240b0ab6240529
Marlon Wiprud <marlonwiprud1@gmail.com>
2024-09-15 13:30:50 -0400

    disable relative line numbers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,6 @@ return {
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
-    ensure_installed = { "javascript" },
+    ensure_installed = { "javascript", "go" },
   },
 }
