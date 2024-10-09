3d871d14b97ba263ea0deb1dbb74cfb7c050c5a7
Marlon Wiprud <marlon@marlonwiprud.com>
2024-10-09 07:17:16 -0400

    feat: setup deno

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,14 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
+        denols = {
+          root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
+          init_options = {
+            enable = true,
+            lint = true,
+            unstable = true,
+          },
+        },
         gopls = {
           settings = {
             gpls = {
